38296157eec36b9b1aba5eb76120b34b3b053f2c
Hồ Đặng Nam Tiến <hodangnamtien@gmail.com>
2024-10-26 22:44:16 +0700

    add sql

--- __xampp/delivery.sql
+++ __xampp/delivery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 25, 2024 at 04:28 PM
+-- Generation Time: Oct 26, 2024 at 05:43 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,28 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `orders` (
   `id` int(10) UNSIGNED NOT NULL,
-  `customer_id` int(10) UNSIGNED NOT NULL,
   `total_price` int(11) NOT NULL,
-  `delivery_id` int(10) UNSIGNED NOT NULL,
   `status` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `customer_id` int(10) UNSIGNED DEFAULT NULL,
+  `delivery_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `customer_id`, `total_price`, `delivery_id`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 300, 1, 'pending', '2024-10-25 14:18:58', '2024-10-25 14:18:58'),
-(2, 1, 300, 1, 'pending', '2024-10-25 14:22:00', '2024-10-25 14:22:00'),
-(3, 1, 300, 1, 'pending', '2024-10-25 14:22:04', '2024-10-25 14:22:04'),
-(4, 1, 300, 1, 'pending', '2024-10-25 14:22:46', '2024-10-25 14:22:46'),
-(5, 1, 300, 1, 'pending', '2024-10-25 14:25:23', '2024-10-25 14:25:23'),
-(6, 1, 300, 1, 'pending', '2024-10-25 14:25:26', '2024-10-25 14:25:26'),
-(7, 1, 300, 1, 'pending', '2024-10-25 14:26:35', '2024-10-25 14:26:35'),
-(8, 1, 300, 1, 'pending', '2024-10-25 14:26:38', '2024-10-25 14:26:38'),
-(9, 1, 300, 1, 'pending', '2024-10-25 14:27:01', '2024-10-25 14:27:01');
+INSERT INTO `orders` (`id`, `total_price`, `status`, `created_at`, `updated_at`, `customer_id`, `delivery_id`) VALUES
+(1, 300, 'delivering', '2024-10-26 12:55:55', '2024-10-26 15:34:06', 2, 2),
+(2, 300, 'pending', '2024-10-26 13:12:39', '2024-10-26 13:12:39', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -60,29 +53,22 @@ INSERT INTO `orders` (`id`, `customer_id`, `total_price`, `delivery_id`, `status
 
 CREATE TABLE `order_items` (
   `id` int(10) UNSIGNED NOT NULL,
-  `order_id` int(10) UNSIGNED NOT NULL,
-  `product_id` int(10) UNSIGNED NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` int(11) NOT NULL,
   `import_price` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `order_id` int(10) UNSIGNED DEFAULT NULL,
+  `product_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `order_items`
 --
 
-INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`, `import_price`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 3, 100, '100', '2024-10-25 14:18:58', '2024-10-25 14:18:58'),
-(2, 2, 1, 3, 100, '100', '2024-10-25 14:22:00', '2024-10-25 14:22:00'),
-(3, 3, 1, 3, 100, '100', '2024-10-25 14:22:04', '2024-10-25 14:22:04'),
-(4, 4, 1, 3, 100, '100', '2024-10-25 14:22:46', '2024-10-25 14:22:46'),
-(5, 5, 1, 3, 100, '100', '2024-10-25 14:25:23', '2024-10-25 14:25:23'),
-(6, 6, 1, 3, 100, '100', '2024-10-25 14:25:26', '2024-10-25 14:25:26'),
-(7, 7, 1, 3, 100, '100', '2024-10-25 14:26:35', '2024-10-25 14:26:35'),
-(8, 8, 1, 3, 100, '100', '2024-10-25 14:26:38', '2024-10-25 14:26:38'),
-(9, 9, 1, 3, 100, '100', '2024-10-25 14:27:01', '2024-10-25 14:27:01');
+INSERT INTO `order_items` (`id`, `quantity`, `price`, `import_price`, `created_at`, `updated_at`, `order_id`, `product_id`) VALUES
+(1, 3, 100, '100', '2024-10-26 12:55:55', '2024-10-26 12:55:55', 1, 1),
+(2, 3, 100, '100', '2024-10-26 13:12:39', '2024-10-26 13:12:39', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -108,7 +94,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `import_price`, `price`, `promotional_price`, `quantity`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Cơm Phúc Lộc Thọ', 'description', 100, 100, 100, 100, 'imageLink', '2024-10-25 07:02:42', '2024-10-25 07:31:42');
+(1, 'Cơm Phúc Lộc Thọ', 'description', 100, 100, 100, 100, 'imageLink', '2024-10-26 12:55:03', '2024-10-26 12:55:03');
 
 -- --------------------------------------------------------
 
@@ -135,7 +121,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `fullname`, `picture`, `role`, `otp_code`, `otp_expired`, `is_verified`, `created_at`, `updated_at`) VALUES
-(1, 'keganvn2@gmail.com', '$2a$10$6/OGSFh4xVhnLSuqtVTake9.vzlseQFetEE3JZcTOxaW7T/zjr9.S', 'Mr A', NULL, 'admin', '208156', '2024-10-25 03:10:50', 0, '2024-10-25 03:00:53', '2024-10-25 03:00:53');
+(1, 'test@gmail.com', '$2a$10$VRlVbDmwZFeCd6ePglGA9eRqEQGGFTCA6VY6iE1QhaMerEGTW8d/.', 'Mr A', NULL, 'customer', '896370', '2024-10-26 13:03:44', 0, '2024-10-26 12:53:44', '2024-10-26 12:53:44'),
+(2, 'keganvn2@gmail.com', '$2a$10$Pp4fJ2KW.6upTOB69ZzHS.f/d/PRjTuAD8B0UTcN/4hWqE4NoC.Ta', 'Mr A', NULL, 'admin', '408980', '2024-10-26 13:03:57', 0, '2024-10-26 12:53:57', '2024-10-26 12:53:57');
 
 --
 -- Indexes for dumped tables
@@ -145,7 +132,9 @@ INSERT INTO `users` (`id`, `email`, `password`, `fullname`, `picture`, `role`, `
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `delivery_id` (`delivery_id`);
 
 --
 -- Indexes for table `order_items`
@@ -175,36 +164,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`delivery_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
 -- Constraints for table `order_items`
 --
 ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `order_items_ibfk_133` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_items_ibfk_134` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
